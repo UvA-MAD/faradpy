2803d44ed614b41a92e5ea1ae70093d6890c55f5
mkuzak <mateusz.kuzak@gmail.com>
2013-11-22 11:46:59 +0100

    Added tests for filter_by_length_fastq.

--- farad/fq.py
+++ farad/fq.py
@@ -41,9 +41,9 @@ def quality_trim_fastq(fastq_in, fastq_out, window_len, min_qual):
     Trim trailing bases which using sliding window.
 
     :param fastq_in: Input fastq file.
-    :type fastq_in: file.
+    :type fastq_in: File handle or path.
     :param fastq_out: Output fastq file.
-    :type fastq_out: file.
+    :type fastq_out: File handle or path.
     :param window_len: Size of sliding window.
     :type window_len: int.
     """
@@ -52,3 +52,22 @@ def quality_trim_fastq(fastq_in, fastq_out, window_len, min_qual):
     trimmed_seqs = [quality_trim_read(s, window_len, min_qual)
                     for s in seqs]
     SeqIO.write(trimmed_seqs, fastq_out, 'fastq')
+
+
+def filter_by_length_fastq(fastq_in, fastq_out, min_len=1, max_len=None):
+    """Fetch only reads of length between min_len and max_len.
+
+    Can be used to either fetching the reads of lentght between min_len
+    and max_len or by specifying only one of the borders to fetch reads longer
+    or shoter than specified value.
+
+    :param fastq_in: Input fastq file.
+    :type fastq_in: File handle or path.
+    :param fastq_out: Output fastq file.
+    :type fastq_out: File handle or path.
+    :param min_len: Minimal length of the read. Defaults to 1.
+    :type min_len: int.
+    :param max_len: Maximal length of the read. Defaults to length of the read.
+    """
+
+
--- tests/test_farad.py
+++ tests/test_farad.py
@@ -1,9 +1,10 @@
 """Tests for farad."""
 import os
+import re
 import sys
 import Bio
 from Bio import SeqIO
-from nose.tools import assert_equal, assert_is_instance, assert_true
+import nose.tools as nt
 
 # import tested package
 sys.path.append('../')
@@ -13,47 +14,73 @@ import farad.fq as ffq
 
 class TestFaradFq:
 
+    # load test fastq
+    test_dir = os.path.split(os.path.abspath(__file__))[0]
+    sample_fq = os.path.join(test_dir, 'sample1.fq')
+
     def setup(self):
         """Create objects needed for testing."""
         pass
 
     def teardown(self):
         """Teardown."""
-        pass
 
-    # load test fastq
-    test_script_path = os.path.split(os.path.abspath(__file__))[0]
-    fq_path = os.path.join(test_script_path, 'sample1.fq')
+        # remove files created during the test
+        # all files created during test should start with 'test_'
+        file_pattern = r'^test_.*\.fq$'
+        for_removal = [f for f in os.listdir(self.test_dir)
+                       if re.search(file_pattern, f)]
+
+        [os.remove(os.path.join(self.test_dir, f)) for f in for_removal]
 
     def test_qualit_trim_read(self):
         """Test quality trimming with sliding window"""
-        sreads = [r for r in SeqIO.parse(self.fq_path, 'fastq')]
+        sreads = [r for r in SeqIO.parse(self.sample_fq, 'fastq')]
 
-        # test trimming
+        # trimming options
         window_len = 3
         min_qual = 21
         # first read of len 82 expected last 2 bases to be trimmed
         # with min_qual=21
         trimmed_sread1 = ffq.quality_trim_read(sreads[0], window_len, min_qual)
-        assert_equal(len(trimmed_sread1), 80)
+        nt.assert_equal(len(trimmed_sread1), 80)
         # second read of len 29 expected to get one base trimmed
         trimmed_sread2 = ffq.quality_trim_read(sreads[1], window_len, min_qual)
-        assert_equal(len(trimmed_sread2), 28)
+        nt.assert_equal(len(trimmed_sread2), 28)
 
         # make sure that the return object is Bio.SeqRecord.SeqRecord
-        assert_is_instance(trimmed_sread2, Bio.SeqRecord.SeqRecord)
+        nt.assert_is_instance(trimmed_sread2, Bio.SeqRecord.SeqRecord)
 
     def test_quality_trim_fastq(self):
         """Test quality trimming of all reads in fastq file."""
         # run trimming on example file
-        fastq_output = os.path.join(self.test_script_path, 'trimmed_reads.fq')
+        fastq_output = os.path.join(self.test_dir, 'test_trimmed.fq')
         window_len = 3
         min_qual = 20
-        ffq.quality_trim_fastq(self.fq_path, fastq_output,
+        ffq.quality_trim_fastq(self.sample_fq, fastq_output,
                                window_len, min_qual)
         # check if file has been created
         file_exists = os.path.exists(fastq_output)
-        assert_true(file_exists)
+        nt.assert_true(file_exists)
 
-        # clean up
-        os.remove(fastq_output)
+    def test_filter_by_length_fastq(self):
+        """Test filtering reads by their length"""
+
+        # if max_len and min_len are not specified all the reads without
+        # filtering should be written to output file
+        fastq_output = os.path.join(self.test_dir, 'test_filt.fq')
+        ffq.filter_by_length_fastq(self.sample_fq, fastq_output)
+
+        # check if the file was created
+        file_exists = os.path.exists(fastq_output)
+        nt.assert_true(file_exists)
+
+        # check if it has the same number of reads
+        n_input_reads = len([line for line
+                             in open(self.sample_fq, 'r').readlines()
+                             if line.startswith('@')])
+
+        n_output_reads = len([line for line
+                             in open(fastq_output, 'r').readlines()
+                             if line.startswith('@')])
+        nt.assert_equal(n_input_reads, n_output_reads)
